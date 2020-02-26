2a3bb3330b37e24814c7e8214aa283af29ccfa6e
X_Shuler_X <egor.pisanov@gmail.com>
2020-02-26 15:01:52 +0300

    Рефакторинг

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -19,13 +19,13 @@ namespace TestConsole
                     rnd.Next(70000, 150000)
                     ));
             const int time_workers_count = 30;
-            for (int i = 0; i < fixed_workers_count; i++)
+            for (int i = 0; i < time_workers_count; i++)
                 workers.Add(new TimeWorker(
                     rnd.Next(20, 35),
                     "TimeWorker" + i,
                     rnd.Next(465, 490)
                     ));
-
+            _Workers = workers.ToArray();
             Pause();
         }
 
